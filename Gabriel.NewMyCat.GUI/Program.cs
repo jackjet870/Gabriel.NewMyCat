5158c791ff8baacd33f3e5d36507159dd14a6c49
zd.yao <zd.yao@jiangtai.com>
2016-03-14 11:21:45 +0800

    add-使用的样例

--- Gabriel.NewMyCat.GUI/Program.cs
+++ Gabriel.NewMyCat.GUI/Program.cs
@@ -15,9 +15,20 @@ namespace Gabriel.NewMyCat.GUI
             MyCat.Instance.NewTransaction("XX", "XX险批单接口");
             //
             MyCat.Instance.LogEvent("是否重新审核", "");
+            CheckEnterprise();
             MyCat.Instance.LogEvent("是否是总社", "");
             MyCat.Instance.LogEvent("调用批单接口", "一般性修改的站内信");
             MyCat.Instance.LogEvent("调用批单接口", "一般性批改的接口");
+            //
+            MyCat.Instance.Complete();
+        }
+
+        static void CheckEnterprise()
+        {
+            MyCat.Instance.NewTransaction("CC", "企业审核");
+            //
+            MyCat.Instance.LogEvent("审核信息", "获取企业审核信息");
+            MyCat.Instance.LogEvent("重新审核", "判断企业是否需要重新审核");
             //
             MyCat.Instance.Complete();
         }
